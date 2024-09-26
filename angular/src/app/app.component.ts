43f1e37bc84830a09f53e7194717f5a2726428d5
DrakeB1234 <92681792+DrakeB1234@users.noreply.github.com>
2024-09-26 01:05:53 -0500

    Added new styling and header component, fixed layout of images on frontend, changed api endpoints to include more info about photo recieved from api

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Photo } from '../Models/photo.model';
-import { AsyncPipe, DecimalPipe } from '@angular/common';
+import { AsyncPipe, DecimalPipe, NgOptimizedImage, NgFor, CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, AsyncPipe, DecimalPipe],
+  imports: [RouterOutlet, AsyncPipe, DecimalPipe, NgOptimizedImage, NgFor, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  http = inject(HttpClient);
+export class AppComponent implements OnInit {
 
-  photos$ = this.getPhotos();
-  
-  private getPhotos(): Observable<Photo[]> {
-    return this.http.get<Photo[]>("https://localhost:7160/api/Photos/GetAllPhotos");
+  private url: string = 'https://localhost:7160/api/Photos/GetAllPhotos';
+  data: Photo[] = [];
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    fetch(this.url)
+      .then((response) => response.json())
+      .then((photosData) => {
+        this.isLoading = false;
+        this.data = photosData
+      });
   }
 }
